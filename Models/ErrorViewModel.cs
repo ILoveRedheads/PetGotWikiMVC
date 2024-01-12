fd4a7de1dc373f52e58350aa1cbae085f561c4b5
ILoveRedheads <64532333+ILoveRedheads@users.noreply.github.com>
2024-01-13 02:51:04 +0300

    .

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoT_WIki.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
